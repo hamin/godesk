0834e0c8c266831b1fc12fbf45595029c6f8caa1
Haris Amin <aminharis7@gmail.com>
2017-06-23 04:23:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# godesk - A quick [Desk](https://github.com/jamesob/desk) workspace launcher with fuzzy filtering
+
+![demo](https://github.com/hamin/godesk/blob/master/demo.gif) 
+
+`godesk` is a simple terminal app to launch a [Desk](https://github.com/jamesob/desk) workspace. It offers a fuzzy filtering menu to select your [Desk](https://github.com/jamesob/desk) file.
+
+`godesk` heavily uses the UI+Fuzzy filtering provided by [go-selecta](https://github.com/thomasboyt/go-selecta) for only one task, launching [Desk](https://github.com/jamesob/desk) workspaces.
+
+
+## Requirements
+
+Install [Desk](https://github.com/jamesob/desk) for your environment
+
+## Installation    
+
+```shell
+  go get -u github.com/hamin/godesk
+```
+
+## Usage
+
+```shell
+godesk
+```
+
+## License
+
+`godesk` is licensed under the MIT License.
+
+## Libraries
+
+* [go-selecta](https://github.com/thomasboyt/go-selecta)
+* [termbox-go](github.com/nsf/termbox-go)
+* [cli](https://github.com/urfave/cli)
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"github.com/thomasboyt/go-selecta/selecta"
+	"github.com/urfave/cli"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+	"unicode"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "godesk"
+	app.Usage = "godesk"
+	app.Version = "1.0.0"
+
+	app.Action = func(c *cli.Context) {
+		goDesk()
+	}
+
+	app.Run(os.Args)
+}
+
+func goDesk() {
+	var (
+		cmdOut []byte
+		err    error
+	)
+	cmdName := "desk"
+	cmdArgs := []string{"list"}
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+	bytes := string(cmdOut)
+
+	// bytes, _ := ioutil.ReadAll(os.Stdin)
+	choices := strings.Split(string(bytes), "\n")
+
+	// create a search
+	initialSearch := "" //c.String("search")
+
+	// set up termbox
+	err2 := termbox.Init()
+	if err2 != nil {
+		panic(err2)
+	}
+
+	_, height := termbox.Size()
+	search := selecta.BlankSearch(choices, initialSearch, height-1)
+
+	termbox.SetInputMode(termbox.InputEsc)
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	DrawApp(search)
+	EventLoop(search)
+
+	termbox.Close()
+
+	binary, lookErr := exec.LookPath("desk")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+	args := []string{"desk", "go", search.SelectedChoice()}
+	env := os.Environ()
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
+}
+
+func EventLoop(s *selecta.Search) {
+	for !s.Done {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyCtrlC:
+				termbox.Close()
+				os.Exit(0)
+			case termbox.KeyBackspace, termbox.KeyBackspace2:
+				s.Backspace()
+			case termbox.KeyEnter:
+				s.Done = true
+			case termbox.KeyArrowDown, termbox.KeyCtrlN:
+				s.Down()
+			case termbox.KeyArrowUp, termbox.KeyCtrlP:
+				s.Up()
+			case termbox.KeyCtrlW:
+				s.DeleteWord()
+			default:
+				char := rune(ev.Ch)
+				if !unicode.IsControl(char) {
+					s.AppendQuery(string(char))
+				} else if ev.Key == termbox.KeySpace {
+					s.AppendQuery(" ")
+				}
+			}
+			termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+			DrawApp(s)
+		}
+	}
+}
+
+// Rendering
+
+func DrawApp(s *selecta.Search) {
+	matchCounter := "(" + (fmt.Sprintf("%v", len(s.Matches))) + ") > "
+	WriteLine(0, matchCounter+s.Query, false)
+	termbox.SetCursor(len(matchCounter)+len(s.Query), 0)
+
+	for i, match := range s.Matches {
+		if i >= s.VisibleChoices {
+			break
+		}
+		choice := match.Value
+		highlight := false
+		if s.Index == i {
+			highlight = true
+		}
+		WriteLine(i+1, choice, highlight)
+	}
+
+	termbox.Flush()
+}
+
+func WriteLine(row int, str string, highlight bool) {
+	bgColor := termbox.ColorDefault
+	textColor := termbox.ColorDefault
+	width, _ := termbox.Size()
+	if highlight {
+		bgColor = termbox.ColorWhite
+		textColor = termbox.ColorBlack
+	}
+
+	for col := 0; col < width; col++ {
+		if col < len(str) {
+			termbox.SetCell(col, row, rune(str[col]), textColor, bgColor)
+		} else {
+			termbox.SetCell(col, row, ' ', textColor, bgColor)
+		}
+	}
+}
