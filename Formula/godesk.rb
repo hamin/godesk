bd81f5a3bea1c0f42a9e91836bc7ac5c6f4a7b5d
Haris Amin <aminharis7@gmail.com>
2017-07-11 13:51:00 -0400

    bumping homebrew version

--- Formula/godesk.rb
+++ Formula/godesk.rb
@@ -1,9 +1,9 @@
 class Godesk < Formula
   desc "A quick Desk workspace launcher with fuzzy filtering"
   homepage "https://github.com/hamin/godesk"
-  url "https://github.com/hamin/godesk/releases/download/v0.1.0/godesk_0.1.0_darwin_amd64.tar.gz"
+  url "https://github.com/hamin/godesk/releases/download/v0.1.1/godesk_0.1.1_darwin_amd64.tar.gz"
   version "0.1.1"
-  sha256 "c0f4f0bf0154e90ab2aba7c4a6b2e78b5a931658"
+  sha256 "55f7554426a4d072c92054298e67ff0210fc1e4a845364342cbfa647a11c6058"
 
   def install
     bin.install "godesk"
